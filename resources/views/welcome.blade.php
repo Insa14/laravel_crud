fe90984dd6163b6c5c59acda84c895e06a5df499
insa14 <kong3361@naver.com>
2022-02-11 22:52:30 +0900

    :wrench: setting
    laravel project setting

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <section class="section-1">
+        <div class="t-container t-mx-auto">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, nulla ipsum dicta magnam quasi unde totam earum quo eos vero perspiciatis voluptatem architecto. Beatae nostrum quidem totam, consequatur cum deserunt?
+            <button class="btn btn-primary">버튼</button>
+        </div>
+    </section>
+</body>
+</html>
